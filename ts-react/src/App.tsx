5a4e32b879eed3481f710a2449205ef2e335152b
Pablo Moreno <ssm.pablo@gmail.com>
2022-06-30 14:30:43 +0200

    passing props

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { TextField } from './TextField';
+import { Counter } from './Counter';
 import { Reducer } from './/Reducer';
+import { TextField } from './TextField';
 
 const App: React.FC = () => {
   return <div>
     <TextField person={{ firstName: 'Bob', lastName: 'Ross' }} handleChange={e => { console.log(e.target.value); }} />
+    <Counter>
+      {({ count, setCount }) => <div>{count}<button onClick={() => setCount(count + 1)}>+</button></div>}
+    </Counter>
     <Reducer />
   </div>
 }
