0bd36693f579d54a2dfc47880c3e8aa60ccb1628
Pablo Moreno <ssm.pablo@gmail.com>
2022-09-25 23:53:01 +0200

    beverage ts interface

--- react/ts-react-hooks/src/components/CustomHookComponent.tsx
+++ react/ts-react-hooks/src/components/CustomHookComponent.tsx
@@ -1,7 +1,20 @@
 import { useState, useEffect } from "react";
 
+export interface Beverage {
+  name: string;
+  producerName: string;
+  beverageName: string;
+  beverageColor: string;
+  beverageStyle: string;
+  producerLocation: string;
+  abv: number;
+  ibu: number;
+  logo: string;
+  level: number;
+}
+
 function useFetchData(url: string) {
-  const [data, dataSet] = useState(null);
+  const [data, dataSet] = useState<Beverage[] | null>(null);
   const [done, doneSet] = useState(false);
 
   useEffect(() => {
