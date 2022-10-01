c3f2973a5ac9d1b8730143449522cce87f62573b
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-01 23:53:40 +0200

    list items

--- react/ts-react-hooks/src/components/MoreReactComponents.tsx
+++ react/ts-react-hooks/src/components/MoreReactComponents.tsx
@@ -1,17 +1,34 @@
+import { ReactNode, ReactElement } from "react";
+
 export interface HeadingProps {
   title: string;
 }
 
 export const Heading = ({ title }: HeadingProps) => {
-  return (
-    <h2>{title}</h2>
-  )
+  return <h2>{title}</h2>
 }
 
-function MoreReactComponents() {
+export const List: <ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[];
+  render: (item: ListItem) => ReactNode;
+}) => ReactElement = ({ items, render }) => {
   return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+};
+
+function MoreReactComponents() {
+  return <div>
     <Heading title="hello" />
-  )
+    <List items={["a", "b", "c"]} render={(str) => <strong>{str}</strong>}/>
+  </div>
 }
 
 export default MoreReactComponents;
